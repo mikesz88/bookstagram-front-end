f2ca8e5b03637ee4d11e5fa25ef754edd7f367ba
michael <michaelsanchez8815@gmail.com>
2022-08-28 21:18:33 -0500

    Update backend endpoint

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://bookstagrambackend.herokuapp.com/api/v1';
+const BASE_URL = 'https://bookstagram-be.cyclic.app/api/v1';
 const URL_AUTH = `${BASE_URL}/auth`;
 const URL_BOOKS = `${BASE_URL}/books`;
 
